ec2f2066965a89d44aaaf825e49fa358c407aa0e
Lynn Wang <lynncwang@gmail.com>
2014-10-21 21:13:08 -0700

    google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,22 @@
 var slides, slides_total, slide_current;
 
 document.addEventListener('DOMContentLoaded', function(){
+
+	document.getElementsByClassName('backward')[0].addEventListener('click', function () {
+      if(slide_current > 0){
+        slide_current--;
+      }else{
+        slide_current = (slides_total - 1);
+      }
+      changePicture(slide_current);
+    });
+
 	slides = document.getElementsByClassName('col-sm-12');
 	slides_total = slides.length;
 	slide_current=0;
+
 	window.setInterval(function(){
-		if(slide_current >= slides_total-1){
+		if(slide_current == slides_total-1){
 			slide_current = 0;
 		} else {
 			slide_current++;
@@ -31,11 +42,26 @@ document.addEventListener('DOMContentLoaded', function(){
 		changePicture(slide_current);
 		console.log(slide_current);
 	}, 3000);
+
+	document.getElementById('forward').addEventListener('click', function () {
+	      // the idea is to decrease slide_current by one, to show the previous image
+	      if(slide_current < (slides_total - 1)){
+	        slide_current++;
+	      }else{
+	        slide_current = 0;
+	      }
+	      changePicture(slide_current);
+		});
+
+	document.getElementsByClassName('restart')[0].addEventListener('click', function () {
+	      slide_current = 0;
+	      changePicture(slide_current);
+	    });
 });
 
-function changePicture(slide) {
-	for(var i = 0; i < slides_total; i++){
-		slides[i].style.display = 'none';
-	}
-	slides[slide].style.display = 'block';
-};
+    function changePicture(slide) {
+		for(var i = 0; i < slides_total; i++){
+			slides[i].style.display = 'none';
+		}
+		slides[slide].style.display = 'block';
+	};
